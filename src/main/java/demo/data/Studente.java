38032efcfcffb28dbf32a5e36ec2866b1a9b50d9
Juri Di Rocco <juri.di.rocco@gmail.com>
2026-04-21 10:02:40 +0200

    Studente data model

---
+++ src/main/java/demo/data/Studente.java
@@ -0,0 +1,25 @@
+package demo.data;
+
+public class Studente {
+    private String nome;
+    private String cognome;
+    private int matricola;
+
+    public Studente(String nome, String cognome, int matricola) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.matricola = matricola;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+}
