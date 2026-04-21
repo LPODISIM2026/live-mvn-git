375541d43e604493892981966184bdd95c167798
Alessio Nunzi <nunzi.alessio2006@gmail.com>
2026-04-21 10:06:34 +0200

    Questo commit chiude la issue #1

--- src/main/java/demo/data/Studente.java
+++ src/main/java/demo/data/Studente.java
@@ -4,11 +4,20 @@ public class Studente {
     private String nome;
     private String cognome;
     private int matricola;
+    private String sesso;
 
     public Studente(String nome, String cognome, int matricola) {
         this.nome = nome;
         this.cognome = cognome;
         this.matricola = matricola;
+    }
+
+    public String getSesso() {
+        return sesso;
+    }
+
+    public void setSesso(String sesso) {
+        this.sesso = sesso;
     }
 
     public String getNome() {
